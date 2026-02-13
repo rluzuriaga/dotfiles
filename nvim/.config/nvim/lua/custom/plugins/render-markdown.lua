d4962d7f244c72f799d9f4106c0d9d5858aa18eb
rluzuriaga <rodrigoluzuriaga@icloud.com>
2026-02-12 20:32:01 -0800

    feat(nvim): Add initial working neovim config

---
+++ nvim/.config/nvim/lua/custom/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'echasnovski/mini.nvim', -- You already have this plugin configured
+  },
+  opts = {}, -- You can later put configuration options here
+}
